ef0f0c98cd9afd30379e1d60010f64845a87b070
smsmsource <100517954+smsmsource@users.noreply.github.com>
2022-03-21 23:12:19 +0200

    Update start.py

--- program/start.py
+++ program/start.py
@@ -104,7 +104,7 @@ async def start_(c: Client, message: Message):
                     InlineKeyboardButton("👥 Support Group", url=f"https://t.me/{GROUP_SUPPORT}"),
                     InlineKeyboardButton("📣 Support Channel", url=f"https://t.me/{UPDATES_CHANNEL}")
                 ],[
-                    InlineKeyboardButton("🌐 Source Code", url="https://github.com/levina-lab/video-stream")
+                    InlineKeyboardButton("🌐 MAZZIKA SOURCE", url="https://t.me/C_MAZZIKA")
                 ],
             ]
         ),
